0ba6c8f5d02b44ee91919f7cfcc4d03888031f4c
Lunei Solei (Bazzite) <68037318+LuneiSolei@users.noreply.github.com>
2025-08-14 14:33:14 -0500

    refactor: Add a new field name, id, to the named tuple of Field
    Signed-off-by: lunei-solei <68037318+Lunei-Solei@users.noreply.github.com>

--- config.py
+++ config.py
@@ -107,6 +107,7 @@ panels = [
 
 # Property Fields
 Field = namedtuple("Field", [
+    "id",
     "label",
     "attr_prefix",
     "ui_data_attr",
@@ -114,12 +115,14 @@ Field = namedtuple("Field", [
     "draw_on"])
 fields = [ # Fields are drawn in the order they are listed here
     Field(
+        id = "name",
         label = "Property Name",
         attr_prefix = None,
         ui_data_attr = None,
         attr_name = "property_name",
         draw_on = 'ALL'),
     Field(
+        id = "label",
         label = "Group Name",
         attr_prefix = None,
         ui_data_attr = None,
@@ -127,12 +130,14 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = 'ALL'
     ),
     Field(
+        id = "type",
         label = "Type",
         attr_prefix = None,
         ui_data_attr = None,
-        attr_name = "property_type",
+        attr_name = "type",
         draw_on = 'ALL'),
     Field(
+        id = "value",
         label = "Value",
         attr_prefix = "value_",
         ui_data_attr = None,
@@ -140,24 +145,28 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = 'ALL'
     ),
     Field(
+        id = "default",
         label = "Default",
         attr_prefix = "default_",
         ui_data_attr = "default",
         attr_name = None,
         draw_on = 'ALL'),
     Field(
+        id = "min",
         label = "Min",
         attr_prefix = "min_",
         ui_data_attr = "min",
         attr_name = None,
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']),
     Field(
+        id = "max",
         label = "Max",
         attr_prefix = "max_",
         ui_data_attr = "max",
         attr_name = None,
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']),
     Field(
+        id = "soft_limits",
         label = "Soft Limits",
         attr_prefix = None,
         ui_data_attr = None,
@@ -165,6 +174,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']
     ),
     Field(
+        id = "soft_min",
         label = "Soft Min",
         attr_prefix = "soft_min_",
         ui_data_attr = "soft_min",
@@ -172,6 +182,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']
     ),
     Field(
+        id = "soft_max",
         label = "Soft Max",
         attr_prefix = "soft_max_",
         ui_data_attr = "soft_max",
@@ -179,6 +190,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']
     ),
     Field(
+        id = "step",
         label = "Step",
         attr_prefix = "step_",
         ui_data_attr = "step",
@@ -186,6 +198,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY', 'INT', 'INT_ARRAY']
     ),
     Field(
+        id = "precision",
         label = "Precision",
         attr_prefix = "precision_",
         ui_data_attr = "precision",
@@ -193,6 +206,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY']
     ),
     Field(
+        id = "subtype",
         label = "Subtype",
         attr_prefix = "subtype_",
         ui_data_attr = "subtype",
@@ -200,6 +214,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = ['FLOAT', 'FLOAT_ARRAY']
     ),
     Field(
+        id = "description",
         label = "Description",
         attr_prefix = None,
         ui_data_attr = "description",
@@ -207,6 +222,7 @@ fields = [ # Fields are drawn in the order they are listed here
         draw_on = 'ALL'
     ),
     Field(
+        id = "library_overridable",
         label = "Library Overridable",
         attr_prefix = None,
         ui_data_attr = None,

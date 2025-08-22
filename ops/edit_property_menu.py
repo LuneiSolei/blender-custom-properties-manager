9255a4b814b643d35e20c23741d6820d4f7db41e
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2025-08-22 16:20:43 -0500

    refactor: move attribute declarations for EditPropertyMenuOperator into EditPropertyMenuOperatorMixin
    Signed-off-by: lunei-solei <68037318+LuneiSolei@users.noreply.github.com>

--- ops/edit_property_menu.py
+++ ops/edit_property_menu.py
@@ -1,88 +1,15 @@
-﻿from typing import Any
-
+﻿# Blender imports
 import bpy
+
+# CPM imports
+from .edit_property_menu_mixin import EditPropertyMenuOperatorMixin
 from ..ui.inputs.field_factory import FieldFactory
 from ..ui.inputs.field import Field
-from bpy.props import (StringProperty, EnumProperty, BoolProperty,
-                       FloatProperty, IntProperty, FloatVectorProperty,
-                       IntVectorProperty, BoolVectorProperty)
 from ..core import GroupData
 from ..core import utils
 from .. import config
 
-# noinspection PyTypeHints
-class EditPropertyMenuOperator(bpy.types.Operator):
-    bl_label = "Edit Property"
-    bl_idname = config.CPM_EDIT_PROPERTY_OP
-    bl_description = "Edit custom property menu"
-
-    # Needed for PyCharm to properly type check against Blender's EnumProperty.
-    # If removed, PyCharm will complain about the PyTypes.
-    # Property attributes
-    data_path:              StringProperty()
-    name:                   StringProperty()
-    type:                   EnumProperty(items = config.CUSTOM_PROPERTY_TYPE_ITEMS)
-    group:                  StringProperty()
-    value: Any
-    use_soft_limits:        BoolProperty()
-    array_length:           IntProperty()
-    precision_float:        IntProperty()
-    precision_float_array:  IntProperty()
-    subtype_float:          EnumProperty(items = config.PROPERTY_SUBTYPE_ITEMS)
-    subtype_float_array:    EnumProperty(items = config.PROPERTY_SUBTYPE_VECTOR_ITEMS)
-    description:            StringProperty()
-    is_overridable_library: BoolProperty()
-
-    # Min values
-    min_float:              FloatProperty()
-    min_float_array:        FloatProperty()
-    min_int:                IntProperty()
-    min_int_array:          IntProperty()
-    soft_min_float:         FloatProperty()
-    soft_min_float_array:   FloatProperty()
-    soft_min_int:           IntProperty()
-    soft_min_int_array:     IntProperty()
-
-    # Max values
-    max_float:              FloatProperty()
-    max_float_array:        FloatProperty()
-    max_int:                IntProperty()
-    max_int_array:          IntProperty()
-    soft_max_float:         FloatProperty()
-    soft_max_float_array:   FloatProperty()
-    soft_max_int:           IntProperty()
-    soft_max_int_array:     IntProperty()
-
-    # Step values
-    step_float:             FloatProperty()
-    step_float_array:       FloatProperty()
-    step_int:               IntProperty()
-    step_int_array:         IntProperty()
-
-    # Default values
-    default_float:          FloatProperty()
-    default_float_array:    FloatVectorProperty()
-    default_int:            IntProperty()
-    default_int_array:      IntVectorProperty()
-    default_bool:           BoolProperty()
-    default_bool_array:     BoolVectorProperty()
-    default_string:         StringProperty()
-    default_data_block:     StringProperty()
-    default_python:         StringProperty()
-
-    # True values
-    value_float:            FloatProperty()
-    value_float_array:      FloatVectorProperty()
-    value_int:              IntProperty()
-    value_int_array:        IntVectorProperty()
-    value_bool:             BoolProperty()
-    value_bool_array:       BoolVectorProperty()
-    value_string:           StringProperty()
-    value_data_block:       StringProperty()
-    value_python:           StringProperty()
-
-    # Misc.
-    _current = {}
+class EditPropertyMenuOperator(bpy.types.Operator, EditPropertyMenuOperatorMixin):
 
     def invoke(self, context, event):
         # Initialize the edit property menu
@@ -138,8 +65,6 @@ class EditPropertyMenuOperator(bpy.types.Operator):
 
     def _get_property_data(self):
         self.value = self._data_object[self.name]
-
-        # noinspection PyTypeChecker
         self.type = utils.get_property_type_from_value(self.value)
 
     def _setup_fields(self):

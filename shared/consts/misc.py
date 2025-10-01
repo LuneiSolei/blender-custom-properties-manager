f701665dc01ab66afd1b8135464f0e30921eca10
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2025-10-01 13:41:51 -0500

    chore: rename module to `custom_properties_manager`

--- shared/consts/misc.py
+++ shared/consts/misc.py
@@ -1,5 +1,5 @@
 ADDON_NAME = "Custom Properties Manager"
-MODULE_NAME = "custom-properties-manager"
+MODULE_NAME = "custom_properties_manager"
 CPM_SERIALIZED_GROUP_DATA = "_cpm_serialized_group_data"
 DEFAULT_GROUP_DATA = "{}"
 KEYS_ATTR = "keys"
